e73415f0083c0349b4bbc69a785289fc65141c6a
Marc <a1627644@student.adelaide.edu.au>
2014-08-29 20:24:42 +0930

    C++ functions

---
+++ cplusplus.cpp
@@ -0,0 +1,52 @@
+// split string by space
+#include <sstream>
+#include <string>
+#include <vector>
+vector<string> split (string &str) {
+	stringstream ss(str);
+	string word;
+	vector<string> tokens;
+	while (ss >> word)
+		tokens.push_back(word);
+	return tokens;
+}
+
+// string to int
+#include <cstdlib>
+#include <string>
+int toInt(string str) {
+	return atoi(str.c_str());
+}
+
+// string to number: int, long, double...
+#include <sstream>
+#include <string>
+float toFloat(string str) {
+	istringstream buffer(str);
+	float value;
+	buffer >> value;
+	return value;
+}
+
+// get digits
+#include <vector>
+vector<int> getDigits (int n) {
+	vector<int> digits;
+	while (n > 0) {
+		digits.push_back(n%10);	// get least sig digit
+		n /= 10;		// shift all digits right
+	}
+	return digits;
+}
+
+// assemble digits into number
+#include <vector>
+int number(vector<int> digits) {
+	int n = 0;
+	int ten = 1;
+	for (int i = 0; i < digits.size(); i++) {
+		n += digits[i]*ten;
+		ten *= 10;
+	}
+	return n;
+}
